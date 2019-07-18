527f2236feb86509b94afb76b19d6a87b437ec8e
dimasm93 <software.dimas_m@icloud.com>
2019-07-18 14:06:48 +0700

    setup maven command plugin docker

---
+++ src/main/java/com/maryanto/dimas/example/springboot2k8sminikubeexample/Springboot2K8sMinikubeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example.springboot2k8sminikubeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2K8sMinikubeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2K8sMinikubeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maryanto/dimas/example/springboot2k8sminikubeexample/Springboot2K8sMinikubeExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maryanto.dimas.example.springboot2k8sminikubeexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2K8sMinikubeExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
